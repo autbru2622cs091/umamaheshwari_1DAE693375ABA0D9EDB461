80cd3fee228707bf99a42bcbcee48d21e0222f4d
autbru2622cs091 <144305912+autbru2622cs091@users.noreply.github.com>
2023-09-07 10:16:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#online python compiler (interpreter)to run python online.
+#write python 3 code in this online editor and run it.
+year=2023
+if(year%4==0):
+  print("leap year")
+else:
+    print("not leap year")
